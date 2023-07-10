f348d15c07294c9f2741ba55109c30f8c9f047bb
Michelle Kyalo <muendikyalo@gmail.com>
2023-07-10 21:30:34 +0300

    Fixed JS lab

--- index.js
+++ index.js
@@ -1,7 +1,5 @@
 const companyName = 'Scuber';
 let mostProfitableNeighborhood;
 mostProfitableNeighborhood = "Chelsea";
-console.log(mostProfitableNeighbourhood==Chelsea)
-let companyCeo 
-companyCeo ='Susan Smith';
-console.log(companyCeo=='Susan Smith')
+let companyCeo ='Susan Smith';
+
